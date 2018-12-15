1f4ab80eba6a5c15c34fdb6ae31b1535a5e20060
stefanJi <408776303@qq.com>
2018-12-15 22:35:59 +0800

    feat: approve merge requst

--- lib/page/tabs/MrTab.dart
+++ lib/page/tabs/MrTab.dart
@@ -2,8 +2,10 @@ import 'dart:convert';
 
 import 'package:F4Lab/gitlab_client.dart';
 import 'package:F4Lab/page/PageMrDetail.dart';
+import 'package:F4Lab/util/widget_util.dart';
 import 'package:F4Lab/widget/comm_ListView.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class MrTab extends CommListWidget {
   final int projectId;
@@ -27,49 +29,64 @@ class _MrState extends CommListState {
   }
 
   Widget _buildItem(mr) {
-    bool assigneed = mr['assignee'] != null;
+    bool assigned = mr['assignee'] != null;
     bool hadDescription =
         mr['description'] != null && (!mr['description'].isEmpty);
     String branch = "${mr['source_branch']} → ${mr['target_branch']}";
+    String uName;
+    if (assigned) {
+      uName = mr['assignee']['username'];
+    }
     var card = Card(
-      child: Column(
-        children: <Widget>[
-          ListTile(
-            onTap: () => _toMrDetatil(mr),
-            title: Text(mr['title']),
-            subtitle: assigneed
-                ? Text.rich(TextSpan(children: <TextSpan>[
-                    TextSpan(
-                        text: mr['assignee']['username'],
+      child: InkWell(
+        onTap: () => _toMrDetatil(mr),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            ListTile(
+              title: Text(mr['title']),
+              subtitle: assigned
+                  ? Text.rich(TextSpan(children: <TextSpan>[
+                      TextSpan(
+                        text: uName.toUpperCase(),
                         style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 16)),
-                    TextSpan(text: " $branch")
-                  ]))
-                : Text(branch),
-            leading: mr['merge_status'] == 'can_be_merged'
-                ? Icon(Icons.done_outline, color: Colors.green)
-                : Icon(
-                    Icons.error,
-                    color: Colors.red,
-                  ),
-            trailing: assigneed
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      TextSpan(text: " $branch")
+                    ]))
+                  : Text(branch),
+              leading: mr['merge_status'] == 'can_be_merged'
+                  ? Icon(
+                      Icons.done_outline,
+                      color: Colors.green,
+                    )
+                  : Icon(
+                      Icons.error,
+                      color: Colors.red,
+                    ),
+              trailing: assigned
+                  ? Padding(
+                      padding: EdgeInsets.only(left: 8.0),
+                      child: loadAvatar(
+                        "${GitlabClient.globalHOST}/uploads/-/system/user/avatar/${mr['assignee']['id']}/avatar.png",
+                        uName,
+                      ),
+                    )
+                  : IgnorePointer(),
+            ),
+            hadDescription
                 ? Padding(
-                    padding: EdgeInsets.only(left: 8.0),
-                    child: CircleAvatar(
-                      radius: 15,
-                      backgroundImage: NetworkImage(
-                          "${GitlabClient.globalHOST}/uploads/-/system/user/avatar/${mr['assignee']['id']}/avatar.png"),
-                    ))
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      mr['description'],
+                    ),
+                  )
                 : IgnorePointer(),
-          ),
-          hadDescription
-              ? ListTile(
-                  onTap: () => _toMrDetatil(mr),
-                  title: Text(mr['description']),
-                )
-              : IgnorePointer(),
-          _MrApprove(mr['project_id'], mr['iid'])
-        ],
+            _MrApprove(mr['project_id'], mr['iid'])
+          ],
+        ),
       ),
     );
     return card;
@@ -92,10 +109,64 @@ class _MrApprove extends StatefulWidget {
   State<StatefulWidget> createState() => _MrApproveState();
 }
 
-class _MrApproveState extends State<_MrApprove> with AutomaticKeepAliveClientMixin {
+/* Approve Item Json Data
+{
+  "id": 5,
+  "iid": 5,
+  "project_id": 1,
+  "title": "Approvals API",
+  "description": "Test",
+  "state": "opened",
+  "created_at": "2016-06-08T00:19:52.638Z",
+  "updated_at": "2016-06-08T21:20:42.470Z",
+  "merge_status": "cannot_be_merged",
+  "approvals_required": 2,
+  "approvals_missing": 2,
+  "approved_by": [],
+  "approvers": [
+    {
+      "user": {
+        "name": "Administrator",
+        "username": "root",
+        "id": 1,
+        "state": "active",
+        "avatar_url": "http://www.gravatar.com/avatar/e64c7d89f26bd1972efa854d13d7dd61?s=80\u0026d=identicon",
+        "web_url": "http://localhost:3000/u/root" 
+      }
+    }
+  ],
+  "approver_groups": [
+    {
+      "group": {
+        "id": 5,
+        "name": "group1",
+        "path": "group1",
+        "description": "",
+        "visibility": "public",
+        "lfs_enabled": false,
+        "avatar_url": null,
+        "web_url": "http://localhost/groups/group1",
+        "request_access_enabled": false,
+        "full_name": "group1",
+        "full_path": "group1",
+        "parent_id": null,
+        "ldap_cn": null,
+        "ldap_access": null
+      }
+    }
+  ]
+}
+*/
+class _MrApproveState extends State<_MrApprove>
+    with AutomaticKeepAliveClientMixin {
   dynamic approve;
 
   _loadApprove() async {
+    if (mounted) {
+      setState(() {
+        approve = null;
+      });
+    }
     final client = GitlabClient.newInstance();
     final data = await client
         .get(
@@ -124,37 +195,108 @@ class _MrApproveState extends State<_MrApprove> with AutomaticKeepAliveClientMix
   }
 
   Widget _buildItem(approve) {
-    bool approvedBy = approve['approved_by'] != null && approve['approved_by'].isNotEmpty;
-    var item = Padding(
-      padding: EdgeInsets.all(10),
-      child: Column(
-        children: <Widget>[
-          approvedBy
-              ? Row(
-                  children: <Widget>[
-                        const Text('Approved by '),
-                      ] +
-                      approve['approved_by'].map<Widget>((item) {
-                        return Padding(
-                            padding: EdgeInsets.all(2),
-                            child: CircleAvatar(
-                              radius: 10,
-                              backgroundImage:
-                                  NetworkImage(item['user']['avatar_url']),
-                            ));
-                      }).toList() +
-                      [const Text(".")])
-              : IgnorePointer(),
-          Row(
-            children: [
-              Text(
-                  "Approvals required ${approve['approvals_required']}, also need ${approve['approvals_left']} approval.")
-            ],
-          )
+    bool hadApproved =
+        approve['approved_by'] != null && approve['approved_by'].isNotEmpty;
+    int hadApproves = approve['approvals_required'];
+    int needApproves = approve['approvals_left'];
+
+    Widget approves;
+    if (hadApproved) {
+      approves = Row(
+        children: approve['approved_by'].map<Widget>((item) {
+              return Padding(
+                padding: EdgeInsets.all(2),
+                child: CircleAvatar(
+                  radius: 10,
+                  backgroundImage: NetworkImage(item['user']['avatar_url']),
+                ),
+              );
+            }).toList() +
+            [const Text("approved.")],
+      );
+    }
+
+    Widget approveTip = Text.rich(
+      TextSpan(
+        text: "Approvals required ",
+        children: [
+          TextSpan(
+            text: "$hadApproves",
+            style: TextStyle(
+              color: Theme.of(context).accentColor,
+            ),
+          ),
+          TextSpan(
+            text: ", also need ",
+          ),
+          TextSpan(
+            text: "$needApproves",
+            style: TextStyle(
+              color: Theme.of(context).accentColor,
+            ),
+          ),
+          TextSpan(
+            text: " approves.",
+          ),
         ],
       ),
     );
-    return item;
+    Widget approveBtn =
+        needApproves != null && needApproves is int && needApproves > 0
+            ? RaisedButton(
+                child: const Text("Approve"),
+                onPressed: () => _approveMr(approve),
+              )
+            : RaisedButton(
+                color: Colors.grey,
+                onPressed: () => _approveMr(approve, isUnApprove: true),
+                child: const Text("UnApprove"),
+              );
+
+    Widget group = Chip(
+      label: Text(
+        "${(approve['approver_groups']).map((item) {
+          return item['group']['name'];
+        }).join(",")}",
+      ),
+    );
+
+    return Padding(
+      padding: EdgeInsets.all(10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: hadApproved
+            ? <Widget>[
+                group,
+                approves,
+                approveTip,
+                approveBtn,
+              ]
+            : <Widget>[
+                group,
+                approveTip,
+                approveBtn,
+              ],
+      ),
+    );
+  }
+
+  _approveMr(approveItem, {bool isUnApprove = false}) async {
+    final endPoint = "projects/${approveItem['project_id']}/merge_requests/"
+        "${approveItem['iid']}/"
+        "${isUnApprove ? "unapprove" : "approve"}";
+    final client = GitlabClient.newInstance();
+    await client.post(endPoint).then((resp) {
+      if (resp.statusCode == 200) {
+        _loadApprove();
+      } else {
+        print("Approve mr error: ${resp.body}");
+        Scaffold.of(context).showSnackBar(SnackBar(
+          content: Text("Action failed. ${resp.body}"),
+          backgroundColor: Colors.red,
+        ));
+      }
+    }).whenComplete(client.close);
   }
 
   @override
---
+++ lib/util/widget_util.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+Widget loadAvatar(url, name, {color = Colors.teal}) {
+  NetworkImage image;
+  try {
+    image = new NetworkImage(url);
+    return CircleAvatar(
+      backgroundImage: image,
+    );
+  } catch (Exception) {
+    print("Load avatar error: $Exception");
+  }
+  return new CircleAvatar(
+    child: Text(
+      name,
+      textAlign: TextAlign.center,
+      overflow: TextOverflow.fade,
+    ),
+    backgroundColor: color,
+  );
+}
