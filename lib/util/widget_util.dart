3ff7ca623d970eec9e6b55f890aa614301ca6630
stefanJi <408776303@qq.com>
2019-05-26 16:37:31 +0800

    fix(loadAvatar): assert args not null

--- lib/util/widget_util.dart
+++ lib/util/widget_util.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 
 Widget loadAvatar(url, name, {color = Colors.teal}) {
-  NetworkImage image;
-  try {
-    image = new NetworkImage(url);
-    return CircleAvatar(
-      backgroundImage: image,
-    );
-  } catch (Exception) {
-    print("Load avatar error: $Exception");
+  assert(name != null);
+  if (url != null) {
+    NetworkImage image;
+    try {
+      image = new NetworkImage(url);
+      return CircleAvatar(
+        backgroundImage: image,
+      );
+    } catch (Exception) {
+      print("Load avatar error: $Exception");
+    }
   }
   return new CircleAvatar(
     child: Text(
